130c898a5d68483093012cc3056c154f41e966e7
plastic041 <plastic041@gmail.com>
2022-02-09 22:15:16 +0900

    meta tags

---
+++ src/components/Head.svelte
@@ -0,0 +1,41 @@
+<script>
+  import { grid } from "~/stores/stores";
+</script>
+
+<svelte:head>
+  <meta name="title" content="Pixer - Paint and share minimal pixel art" />
+  <meta
+    name="description"
+    content="Pixer is a simple pixel art editor. You can easily paint and share some minimal pixel art."
+  />
+  <meta name="copyright" content="snubi" />
+
+  <meta property="og:type" content="website" />
+  <meta property="og:url" content="https://pixer-phi.vercel.app/" />
+  <meta
+    property="og:title"
+    content="Pixer - Paint and share minimal pixel art"
+  />
+  <meta
+    property="og:description"
+    content="Pixer is a simple pixel art editor. You can easily paint and share some minimal pixel art."
+  />
+  <meta
+    property="og:image"
+    content={`https://og-image-theta-ruddy.vercel.app/${$grid}.jpeg?size=16`}
+  />
+  <meta property="twitter:card" content="summary_large_image" />
+  <meta property="twitter:url" content="https://pixer-phi.vercel.app/" />
+  <meta
+    property="twitter:title"
+    content="Pixer - Paint and share minimal pixel art"
+  />
+  <meta
+    property="twitter:description"
+    content="Pixer is a simple pixel art editor. You can easily paint and share some minimal pixel art."
+  />
+  <meta
+    property="twitter:image"
+    content={`https://og-image-theta-ruddy.vercel.app/${$grid}.jpeg?size=16`}
+  />
+</svelte:head>
