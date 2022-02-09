2126e8094c027544dc23f08b3bc1711684f9c87a
plastic041 <plastic041@gmail.com>
2022-02-09 21:02:14 +0900

    use routing

---
+++ src/routes/[text].svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import Page from "~/components/Page.svelte";
+  import { page } from "$app/stores";
+  import { grid } from "~/stores/stores";
+  import { onMount } from "svelte";
+
+  onMount(() => {
+    const text = $page.params.text;
+    grid.set(text);
+  });
+</script>
+
+<Page />
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,42 +1,5 @@
 <script>
-  import Grid from "~/components/Grid.svelte";
-  import Palette from "~/components/Palette.svelte";
-  import Preview from "~/components/Preview.svelte";
-  import Keys from "~/components/Keys.svelte";
+  import Page from "~/components/Page.svelte";
 </script>
 
-<svelte:head>
-  <link
-    rel="stylesheet"
-    href="https://unpkg.com/modern-css-reset/dist/reset.min.css"
-  />
-</svelte:head>
-
-<Keys />
-<main class="container">
-  <div class="header">
-    <Palette />
-    <Preview />
-  </div>
-  <Grid />
-</main>
-
-<style>
-  .container {
-    padding: 1rem 0;
-    display: flex;
-    flex-direction: column;
-    gap: 1rem;
-    margin: 0 auto;
-    width: 400px;
-    max-width: 600px;
-  }
-
-  .header {
-    width: 100%;
-    height: 100px;
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-  }
-</style>
+<Page />
